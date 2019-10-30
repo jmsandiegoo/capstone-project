dcb9868a358e1771ae0972fb10ed986b55a2dec9
John Michael San Diego <jmsandiego0710@gmail.com>
2019-10-30 13:10:49 +0800

    Added basic folder strcuture and include helper

---
+++ helpers/include.php
@@ -0,0 +1,8 @@
+<?php
+
+function subview($file) {
+    $file = __DIR__ . '/../views/sub-views/'.$file;
+    include $file;
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+    include 'helper/helper.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    
+
+
+
+</html>
