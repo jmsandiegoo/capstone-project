0ed8f6e46d25502f3c1b8ff95dce160b3ff4c732
John Michael San Diego <jmsandiego0710@gmail.com>
2019-11-03 17:00:32 +0800

    Added license key

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,11 @@ alert('hello');
 // Index.php javascript fullpagejs
 
 new fullpage('#fullpage', {
-	//options here
+    //options here
+    licenseKey: 'CD50B0E4-F5FD4F8C-AF7C78F1-5B645B1D',
 	autoScrolling:true,
 	scrollHorizontally: true
 });
 
 //methods
-fullpage_api.setAllowScrolling(false);
+fullpage_api.setAllowScrolling(true);
