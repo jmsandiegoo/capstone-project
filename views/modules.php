97a30d10fd5ae06387e1b9fb6c24780ef4694d51
Yeow Keng Chiu <s10182660@connect.np.edu.sg>
2019-11-10 02:32:48 +0800

    Basic code for module to extract modules from database added

--- views/modules.php
+++ views/modules.php
@@ -1,13 +1,28 @@
 <?php 
+    include_once __DIR__.'/../helpers/mysql.php';
     include_once __DIR__.'/../helpers/helper.php';
     $helper = new Helper();
+
+    // Retrieving the course details
+    $db = new Mysql_Driver();
+    $db->connect();
+
+    $sql = "SELECT * FROM Module m INNER JOIN CourseModule cm ON m.module_id = cm.module_id WHERE cm.id= " . $_GET['cid'];
+    $result = $db->query($sql);
+
+    $db->close();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <?php include $helper->subviewPath('header.php') ?>
     <main class="container">
-        <h1>Course odules</h1>
+        <h1>Course Modules</h1>
+        <?php while ($row = $db->fetch_array($result)): ?>
+        <p><?php echo "+ " . $row['module_name']?></p>
+        
+        <?php endwhile; ?>
+                                    
     </main>
     <?php include $helper->subviewPath('footer.php') ?>
 
