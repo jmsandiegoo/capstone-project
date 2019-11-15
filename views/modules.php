733d9a3ffd425e8860402906109ffcd549120288
Avin <s10178174@connect.np.edu>
2019-11-15 15:16:03 +0800

    added filter into modules page

--- views/modules.php
+++ views/modules.php
@@ -2,12 +2,16 @@
     include_once __DIR__.'/../helpers/mysql.php';
     include_once __DIR__.'/../helpers/helper.php';
     $helper = new Helper();
-	  $id = $_GET['id'];
-    
-    $years = [];
+	  //$id = $_GET['id'];
+    $id = 1;
     // Retrieving the course information
     $db = new Mysql_Driver();
     $db->connect();
+    // print "<pre>";
+    // print_r($courseInfo);
+    // print "</pre>";
+    // Retrieving the module year
+        
     $sql = "SELECT * FROM Course WHERE id=$id";
     $courseInfoResult = $db->query($sql);
     $courseInfo = [];
@@ -15,27 +19,14 @@
       $courseInfo[] = $row;
     }
     $courseInfo = $courseInfo[0];
-
-    // print "<pre>";
-    // print_r($courseInfo);
-    // print "</pre>";
-    // Retrieving the module year
-    $sql2 = "SELECT DISTINCT module_year FROM CourseModule WHERE id=$id ORDER BY module_year ASC";
-    $moduleYearResult = $db->query($sql2);
-    $moduleYear = [];
-    while ($row = $db->fetch_array($moduleYearResult)) {
-      $moduleYear[] = $row;
-    }
-    $db->close();
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <?php include $helper->subviewPath('header.php') ?> 
     <main class="modules-container">
       <?php 
-        $db->connect();
-        $sql3 = "SELECT * FROM Item WHERE course_id = " . $id . " AND item_path LIKE '%BG_1.jpg'";
-        $courseItemResult = $db->query($sql3);
+        $sql2 = "SELECT * FROM Item WHERE course_id = " . $id . " AND item_path LIKE '%BG_1.jpg'";
+        $courseItemResult = $db->query($sql2);
         $backgroundPath = '';
         while ($row = $db->fetch_array($courseItemResult)) {
           $backgroundPath = '../' . $row['item_path'];
@@ -48,7 +39,8 @@
       </section>
       <section class="module-year-description container">
         <!-- Description is still hardcoded -->
-        <?php foreach ($courseInfo as $key => $value): ?>
+        <?php
+        foreach ($courseInfo as $key => $value): ?>
 
           <?php if ($key === 'course_year1_description'): ?>
             <h2>Year 1</h2>
@@ -63,28 +55,71 @@
             continue;
           endif;?>
         <?php endforeach; ?>
+
+
+        
       </section>
 
       <section class="container">
-        <?php foreach ($moduleYear as $key => $row){
-          $moduleYear = $row['module_year'];
-          echo "<button type=\"button\" class=\"collapsible\">".$moduleYear."</button><div class=\"content\">";
-          $db->connect();
-          $sql4 = "SELECT * FROM Module m INNER JOIN CourseModule cm ON m.module_id = cm.module_id WHERE cm.id= $id AND cm.module_year ='$moduleYear' ORDER BY module_name ASC";
-          $courseModuleResult = $db->query($sql4);
-          while ($row2 = $db->fetch_array($courseModuleResult)){
-            echo "<p>".$row2['module_name']."</p></br>";
-          }
-          echo "</div>";
+        <!-- filter buttons !-->
+        <div id="myBtnContainer">
+          <button class="btn active" onclick="filterSelection('all')"> Show all</button>
+          <?php 
+            //Get the job names
+            $sql3 = "SELECT j.job_name FROM Job j INNER JOIN CourseJob cj ON j.job_id = cj.job_id WHERE cj.id = $id";
+            $jobNameResult = $db->query($sql3);
+            $jobName = [];
+            while ($row = $db->fetch_array($jobNameResult)) {
+              $jobName[] = $row;
+            }
+            foreach ($jobName as $key => $row)
+            {
+              $jobName = $row['job_name'];
+              echo "<button class=\"btn\" onclick=\"filterSelection('".$jobName."')\">".$jobName."</button>";
+            }
+          ?>
+        </div>
+          </br>
+        <!--  !-->
+        <div id="tableContainer">
+          <?php
+            //Get module_year for IT Course
+            $sql4 = "SELECT DISTINCT module_year FROM CourseModule WHERE id=$id ORDER BY module_year ASC";
+            $moduleYearResult = $db->query($sql4);
+            $moduleYear = [];
+            while ($row = $db->fetch_array($moduleYearResult)) {
+              $moduleYear[] = $row;
+            }
+            foreach ($moduleYear as $key => $row){
+              $moduleYear = $row['module_year'];
+              echo "<button type=\"button\" class=\"collapsible\">".$moduleYear."</button><div class=\"content\">";
+              //Get module name to display and module id for filtering
+              $sql5 = "SELECT * FROM Module m INNER JOIN CourseModule cm ON m.module_id = cm.module_id WHERE cm.id= $id AND cm.module_year ='$moduleYear' ORDER BY module_name ASC";
+              $courseModuleResult = $db->query($sql5);
+              while ($row2 = $db->fetch_array($courseModuleResult)){ 
+                //get names of jobs for the modules
+                $sql6 = "SELECT * FROM Module m INNER JOIN CourseModule cm ON m.module_id = cm.module_id INNER JOIN ModuleJob mj ON mj.module_id=m.module_id INNER JOIN Job j ON j.job_id=mj.job_id WHERE cm.id=$id AND m.module_id='".$row2['module_id']."'ORDER BY module_name ASC";
+                $filterJobNameResult = $db->query($sql6);
+                $filterJobName = array();
+                while ($row4 = $db->fetch_array($filterJobNameResult)){
+                  array_push($filterJobName,$row4['job_name']);
+                } 
+              
+                $filterJobNameString = implode( ", ", $filterJobName );
+                echo "<div class=\"filterDiv ".$filterJobNameString." \">".$row2['module_name']."</div></br>";
+              }
+
+              echo "</div>";
+            }
             $db->close();
-          }
-        ?>
+          ?>
+        </div>
       </section>
     </main>
-    
     <?php include $helper->subviewPath('footer.php') ?>
-
 </html>
+
+
 <style>
     table {
     font-family: arial, sans-serif;
@@ -119,12 +154,52 @@
 
     .content {
       padding: 0 18px;
-      display: none;
+      display: block;
       overflow: hidden;
       background-color: #f1f1f1;
     }
+
+    /* Buttons css*/
+    
+    .filterDiv {
+      float: left
+      text-align: center;
+      margin: 2px;
+      display: block;
+      color: #D3D3D3;
+    }
+
+    .show {
+      display: block;
+      color: black;
+    }
+
+    .container {
+      margin-top: 20px;
+      overflow: hidden;
+    }
+
+    /* Style the buttons */
+    .btn {
+      border: none;
+      outline: none;
+      padding: 12px 16px;
+      background-color: #f1f1f1;
+      cursor: pointer;
+    }
+
+    .btn:hover {
+      background-color: #ddd;
+    }
+
+    .btn.active {
+      background-color: #666;
+      color: white;
+    }
     </style>
+
     <script>
+      /*collapsible button(module_year)*/
       var coll = document.getElementsByClassName("collapsible");
       var i;
 
@@ -132,11 +207,55 @@
         coll[i].addEventListener("click", function() {
           this.classList.toggle("active");
           var content = this.nextElementSibling;
-          if (content.style.display === "block") {
-            content.style.display = "none";
-          } else {
+          if (content.style.display === "none") {
             content.style.display = "block";
+          } else {
+            content.style.display = "none";
           }
         });
         }
-    </script>
+        
+        /* filter */
+        filterSelection("all")
+        function filterSelection(c) {
+          var x, i;
+          x = document.getElementsByClassName("filterDiv");
+          if (c == "all") c = "";
+          for (i = 0; i < x.length; i++) {
+            w3RemoveClass(x[i], "show");
+            if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+          }
+        }
+
+        function w3AddClass(element, name) {
+          var i, arr1, arr2;
+          arr1 = element.className.split(" ");
+          arr2 = name.split(" ");
+          for (i = 0; i < arr2.length; i++) {
+            if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+          }
+        }
+
+        function w3RemoveClass(element, name) {
+          var i, arr1, arr2;
+          arr1 = element.className.split(" ");
+          arr2 = name.split(" ");
+          for (i = 0; i < arr2.length; i++) {
+            while (arr1.indexOf(arr2[i]) > -1) {
+              arr1.splice(arr1.indexOf(arr2[i]), 1);     
+            }
+          }
+          element.className = arr1.join(" ");
+        }
+
+        // Add active class to the current button (highlight it)
+        var btnContainer = document.getElementById("myBtnContainer");
+        var btns = btnContainer.getElementsByClassName("btn");
+        for (var i = 0; i < btns.length; i++) {
+          btns[i].addEventListener("click", function(){
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+          });
+        }
+</script>
