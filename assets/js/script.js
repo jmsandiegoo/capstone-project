f94a374bbddca1a2b9719d306dd33da0afb82a61
John Michael San Diego <jmsandiego0710@gmail.com>
2019-11-17 20:19:40 +0800

    Fixed the highlighting of cards when filter button is pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -116,8 +116,8 @@ function filterSelection(c) {
     x = document.getElementsByClassName("filterDiv");
     if (c == "all") c = "";
     for (i = 0; i < x.length; i++) {
-        w3RemoveClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+        w3RemoveClass(x[i], "highlight");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "highlight");
     }
 }
 
