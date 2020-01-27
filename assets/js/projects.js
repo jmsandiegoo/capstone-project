53d4581b1d5bcaa97c2e8e6885e681ab35dc0339
John Michael San Diego <jmsandiego0710@gmail.com>
2020-01-27 12:18:24 +0800

    Fixed bugs in project carousel

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -22,11 +22,67 @@ function getUrlParameter(name) {
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
 
+var drag = false;
+
+$(document).ready(function(){
+    $('.carousel').slick({
+    dots: true,
+    centerMode: false,
+    infinite: true,
+    speed: 500,
+    autoplay: false,
+    slidesToShow: 3,
+    draggable: true,
+    slidesToScroll: 3,
+    cssEase: 'linear',
+    // swipeToSlide: true,
+    responsive: [
+        {
+        breakpoint: 1024,
+        settings: {
+            centerMode: false,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            centerMode: true,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+        }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+    });
+
+    $('.carousel').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        console.log('Before Change')
+        drag = true;
+      });
+      
+      // On before slide change
+      $('.carousel').on('afterChange', function(event, slick, currentSlide){
+        console.log('After Change');
+        drag = false;
+      });
+});
+
 // Adding on click in project poster
 var projectPoster = document.querySelectorAll(".btcarousel-item");
 for (var i = 0; i < projectPoster.length; i++) {
     projectPoster[i].addEventListener("click", function() {
-        console.log(this.dataset.id);
+        if (drag){
+            console.log("click fail");
+            return;
+        }
         var project_id = this.dataset.id
         getAjaxProjectInfo(project_id);
     });
